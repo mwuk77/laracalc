88fa24dc8cfa72169854527deb596becda3ecba2
mark <mark@e525>
2020-08-18 16:33:50 +0100

    Convert an authentication exception into an unauthenticated response.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,4 +52,16 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+    
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticated.'], 401);
+    }    
 }
