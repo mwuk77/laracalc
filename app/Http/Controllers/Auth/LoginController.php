f42f4ca34ba3269f2a2c7aac5038bb1891160210
mark <mark@e525>
2020-08-18 16:31:38 +0100

    LoginController for vending API tokens.

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request))
+        {
+            $user = $this->guard()->user();
+            $user->generateToken();
+
+            return response()->json([
+                        'data' => $user->toArray(),
+            ]);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function logout()
+    {
+        $user = Auth::guard('api')->user();
+
+        if ($user)
+        {
+            $user->api_token = null;
+            $user->save();
+        }
+
+        return response()->json(['data' => 'User logged out.'], 200);
+    }
+
+    protected function sendFailedLoginResponse()
+    {
+        $errors = ['error' => trans('auth.failed')];
+
+        return response()->json($errors, 422);
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+
+}
