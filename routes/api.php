fdd20546b96df611bdbe25c92e1c8f8792d5443a
mark <mark@e525>
2020-08-18 15:16:18 +0100

    Added /operation POST endpoint with parameter validation.

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,48 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OperationController;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('operation', function(Request $request) {
+    $operationController = new OperationController();
+    
+    
+    /**
+     * This could become a lot of boiler-plate with more routes added.
+     */
+    $validator = Validator::make($request->all(), ['expression' => ['required']]);
+    
+    if($validator->fails())
+    {
+        return response()->json(
+            [
+                'errors' => $validator->errors()
+            ],
+            400
+        );
+    }
+    else
+    {
+        return response()->json(
+            [
+                'result' => $operationController->execute($request)
+            ],
+            200
+        );
+    }
 });
