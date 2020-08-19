76103b71810bb007036d24af764bb4422fdb884f
mark <mark@e525>
2020-08-19 08:32:38 +0100

    Moved credentials to .env files.

---
+++ config/user.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'admin_email' => env('USER_ADMIN_EMAIL'),
+    'admin_password' => env('USER_ADMIN_PASSWORD'),
+    'front_email' => env('USER_FRONT_EMAIL'),
+    'front_password' => env('USER_FRONT_PASSWORD'),
+];
