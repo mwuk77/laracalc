1042ab0a25a6123a3a654f0f676b9e07b8bc77e4
mark <mark@e525>
2020-08-22 20:45:51 +0100

    Changed to import Vue calculator.

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -1,28 +1,9 @@
-
-<div class="calculator">
-    <input class="display" type="text">
-    <div class="buttons">
-        @foreach ($buttons as $button)<button data-key='{{ $button }}'>{{ $button }}</button>@endforeach
-    </div>
+<div  id="app">
+    <calculator
+        :buttons="{{ json_encode($buttons) }}"
+        api-base="{!! URL::to('/') !!}/api"
+        display-length="8"
+        id="{!! Config::get('user.front_email') !!}"
+        pw="{!! Config::get('user.front_password') !!}"
+    ></calculator>
 </div>
-
-<script type="text/javascript" src="{{ asset('js/components/calculator.js')}}"></script>
-<script type='text/javascript'>
-    setKeys({!! json_encode($buttons) !!});
-    
-    setApiBase("{!! URL::to('/') !!}/api");
-    
-    /*
-     * Laravel auth guard uses plain password to check against the hash.
-     * I'm uncomfortable sending it like this, but with SSL the connection 
-     * should be private.
-     */
-    setCreds("{!! Config::get('user.front_email') !!}", 
-        "{!! Config::get('user.front_password') !!}");
-
-    bindKeys(keyboardJS);
-    
-    getToken();
-    
-    updateDisplay('');
-</script>
