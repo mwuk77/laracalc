0fe6725461a5894cdc1b5bf9091ae939e111ffe6
mark <mark@e525>
2020-08-27 09:25:07 +0100

    Added KeysSeeder and LayoutKeysSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersSeeder::class);
         $this->call(LayoutsSeeder::class);
+        $this->call(KeysSeeder::class);
+        $this->call(LayoutKeysSeeder::class);
     }
 }
